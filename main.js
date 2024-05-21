9d790a2da5978fabe323228f4ad0e71f0af8178c
haidar-miqdad <haidarmiqdad2004@gmail.com>
2024-05-21 16:27:16 +0700

    change ability

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
     Typed Js 
 ====================== */
 const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Content Creator", "UI/UX Designer"],
+  strings: ["Mobile Developer", "web Developer",],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
